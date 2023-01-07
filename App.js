0d1bdb0881f2931711df5630ac8113a1e62eb92f
Arthur Silva <arthur10.silva312@gmail.com>
2023-01-07 14:37:56 -0300

    fix: Asyncstorage context removido

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {UserProvider} from './src/context/User';
-import { AsyncStorageProvider } from './src/context/ManageAsyncStorage';
+import { NavigationContainer } from '@react-navigation/native';
+import { UserProvider } from './src/context/User';
 import Routes from './src/routes';
 
 export default function App() {
   return (
     <NavigationContainer>
       <UserProvider>
-    <AsyncStorageProvider>
-    <Routes />
-    </AsyncStorageProvider>
+        <Routes />
       </UserProvider>
     </NavigationContainer>
   );
