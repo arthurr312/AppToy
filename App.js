88fc81ed583864217d2bbe2813fdb54133798b61
Arthur Silva <arthur10.silva312@gmail.com>
2023-01-26 21:17:15 -0300

    feat: adição de estilo na statusBar da aplicação

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { UserProvider } from './src/context/User';
+import {NavigationContainer} from '@react-navigation/native';
+import {UserProvider} from './src/context/User';
 import Routes from './src/routes';
+import {StatusBar} from 'react-native';
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar barStyle={'light-content'} backgroundColor="#000" />
       <UserProvider>
         <Routes />
       </UserProvider>
